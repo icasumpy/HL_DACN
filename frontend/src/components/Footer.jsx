297d081417b832990023f5395a43eef51c4dbb16
Phan Thi Khanh Ly <phanthikhanhly2026520@gmail.com>
2026-05-02 23:38:24 +0700

    push

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
-      
+      <div>
+
+        {/* ------ Left Section ------ */}
+        <div>
+            <img src={assets.logo} alt="" />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+
+        {/* ------ Center Section ------ */}
+        <div>
+            <p>COMPANY</p>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact us</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        {/* ------ Right Section ------ */}
+        <div>
+            
+        </div>
+
+      </div>
     </div>
   )
 }
 
 export default Footer
+//1.43.40
