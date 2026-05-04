aa125d60651d86084c5caa637c2a6c9089608817
Phan Thi Khanh Ly <phanthikhanhly2026520@gmail.com>
2026-05-04 20:35:51 +0700

    .

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,19 +3,19 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
         {/* ------ Left Section ------ */}
         <div>
-            <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <img className='mb-5 w-40' src={assets.logo} alt="" />
+            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
 
         {/* ------ Center Section ------ */}
         <div>
-            <p>COMPANY</p>
-            <ul>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-2 text-gray-600'>
                 <li>Home</li>
                 <li>About us</li>
                 <li>Contact us</li>
@@ -25,13 +25,20 @@ const Footer = () => {
 
         {/* ------ Right Section ------ */}
         <div>
-            
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-2 text-gray-600'>
+              <li>+84 899 152 747</li>
+              <li>lyptk.23it@vku.udn.vn</li>
+            </ul>
         </div>
-
+      </div>
+      {/* --------- Copyright Text --------- */}
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>23JIT - yeyeye</p>
       </div>
     </div>
   )
 }
 
 export default Footer
-//1.43.40
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
+
+    const { speciality } = useParams()
+    const [fillterDoc,setFillterDoc] = useState([])
+    const navigate = useNavigate()
+
+    const {doctors} = useContext(AppContext)
+
+
+    const applyFilter = () => {
+        if (speciality) {
+            setFillterDoc(doctors.filter(doc => doc.speciality === speciality))
+        } else {
+            setFillterDoc(doctors)
+        }
+    }
+
+    useEffect(()=> {
+        applyFilter()
+    },[doctors,speciality])
+
     return (
         <div>
-
+            <p>Browse through the doctors specialist.</p>
+            <div>
+                <div>
+                    <p>General physician</p>
+                    <p>Gynecologist</p>
+                    <p>Dermatologist</p>
+                    <p>Pediatricians</p>
+                    <p>Neurologist</p>
+                    <p>Gastroenterologist</p>
+                </div>
+                <div className='w-full grid-cols-auto gap-4 gap-y-6'>
+                    {
+                        fillterDoc.map((item,index)=>(
+                            <div onClick={()=>navigate('/appointmant/${item._id}')} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
+                                <img className='bg-blue-50' src="item.image" alt="" />
+                                <div className='p-4'>
+                                    <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                                    </div>
+                                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                                    <p className='text-gray-600 text-sm'>{item.speciality}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     ) 
 }
 
 export default Doctors
+//2:00:00
